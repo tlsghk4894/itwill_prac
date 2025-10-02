1dc3e1b4c829a5e41ab6dd213fdaed96d5c46b8e
tlsghk4894 <83351933+tlsghk4894@users.noreply.github.com>
2025-10-02 10:35:47 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,9 @@
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE,
+case
+    when status = 'sale' then '판매중'
+    when status = 'reserved' then '예약중'
+    when status = 'done' then '거래완료'
+end as 'STATUS'
+from used_goods_board
+where created_date = '2022-10-05'
+order by board_id desc;
